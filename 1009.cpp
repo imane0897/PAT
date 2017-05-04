16f53a611d13db559b7dcd5821e2e8274682342d
Serene <imane0897@gmail.com>
2017-05-04 17:27:06 +0800

    no message

--- 1009.cpp
+++ 1009.cpp
@@ -2,55 +2,87 @@
 #include <stdio.h>
 #include <array>
 #include <cstring>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
+struct poly {
+    int expo;
+    double coe;
+} ;
+
+poly a[15], b[15], c[100];
+int flag = -1;
+
+void refresh_c(int expo, double coe) {
+    for (int i = 0; i < 100; i++) {
+        if (c[i].expo == expo) {
+            c[i].coe += coe;
+            return;
+        }
+    }
+    c[++flag].expo = expo;
+    c[flag].coe = coe;
+}
+
+bool mysortfunc(const poly a, const poly b) {
+    if (a.expo > b.expo) {
+        return true;
+    }
+    return false;
+}
+
+
 int main(void) {
-    int k, i, j, index, max_index = 0;
-    double a[1005], b[1005], c[1005];
+    int k1, k2, i, j;
 
-    memset(a, 0, 1005);
-    memset(b, 0, 1005);
-    memset(c, 0, 1005);
-
-    scanf("%d", &k);
-    for (i = 0; i < k; i++) {
-        scanf("%d", &index);
-        scanf("%lf", &a[index]);
-        if (index > max_index) {
-            max_index = index;
-        }
+    // init
+    for (i = 0; i < 15; i++) {
+        a[i].expo = 0; a[i].coe = 0;
+        b[i].expo = 0; b[i].coe = 0;
     }
-    scanf("%d", &k);
-    for (i = 0; i < k; i++) {
-        scanf("%d", &index);
-        scanf("%lf", &b[index]);
-        if (index > max_index) {
-            max_index = index;
-        }
+    for (i = 0; i < 100; i++) {
+        c[i].expo = 0; c[i].coe = 0;
     }
+    //////
 
-    for (i = 0; i <= max_index; i++) {
-        for (j = 0; j<= max_index; j++) {
-            index = i + j;
-            c[index] += a[i] * b[j];
+    // input
+    scanf("%d", &k1);
+    for (i = 0; i < k1; i++) {
+        scanf("%d%lf", &a[i].expo, &a[i].coe);
+    }
+    scanf("%d", &k2);
+    for (i = 0; i < k2; i++) {
+        scanf("%d%lf", &b[i].expo, &b[i].coe);
+    }
+    //////
+
+    int expo;
+    double coe;
+    for (i = 0; i < k1; i++) {
+        for (j = 0; j < k2; j++) {
+            expo = a[i].expo + b[j].expo;
+            coe = a[i].coe * b[j].coe;
+            refresh_c(expo, coe);
         }
     }
 
     int count = 0;
-    for (i = 0; i <= 2*max_index; i++) {
-        if (c[i] > 0.001) {
-            count ++;
+    for (i = 0; i < 100; i++) {
+        if (abs(c[i].coe) > 0.005) {
+            count++;
         }
     }
+    std::sort(c, c+100, mysortfunc);
 
     // output
     printf("%d", count);
-    for (i = max_index; i >= 0; i--) {
-        if (c[i] > 0.001) {
-            printf(" %d", i);
-            printf(" %.1lf", c[i]);
+    for (i = 0; i < 100; i++) {
+        if (abs(c[i].coe) > 0.005) {
+            printf(" %d %.1lf", c[i].expo, c[i].coe);
         }
     }
     printf("\n");
+    //////
     return 0;
 }
--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ FILE = 1009
 all: clean
 
 build:
-	g++ -Wall $(FILE).cpp -o $(FILE).out
+	g++ $(FILE).cpp -o $(FILE).out
 
 run: build
 	./$(FILE).out < INPUT
