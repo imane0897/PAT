8128fc6660380113d4a8c4f95b48f5b60d7e10eb
Serene <imane0897@gmail.com>
2017-05-05 12:43:51 +0800

    1003 not sovled

--- 1003.cpp
+++ 1003.cpp
@@ -6,33 +6,83 @@
 using namespace std;
 
 const int MAXN = 505;
-int cost[MAXN][MAXN];
-int path_2_des[MAXN];         // number of paths from element i to element des
-int path_cost[MAXN];          // cost of element i to element j
+const int INF = 0x3f3f3f;
+bool vis[MAXN];
+int pre[MAXN];
+int path_count[MAXN];
 
-void find_all_path() {
-
+void Dijkastra(const int cost[][MAXN], int lowcost[], int n, int beg) {
+    for (int i = 0; i < n; i++) {
+        lowcost[i] = INF; vis[i] = false; pre[i] = -1; path_count[i] = 0;
+    }
+    lowcost[beg] = 0;
+    for (int j = 0; j < n; j++) {
+        int k = -1;
+        int Min = INF;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i] && lowcost[i] < Min) {
+                Min = lowcost[i];
+                k = i;
+            }
+        }
+        if (k == -1) break;
+        vis[k] = true;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i] && lowcost[k] + cost[k][i] < lowcost[i]){
+                lowcost[i] = lowcost[k] + cost[k][i];
+                pre[i] = k;
+            } else if (!vis[i] && lowcost[k] + cost[k][i] == lowcost[i] && k != beg) {
+                lowcost[i] = lowcost[k] + cost[k][i];
+                pre[i] = k;
+                printf("lowcost[%d] = %d, cost[%d][%d] = %d, lowcost[%d] = %d\n", k, lowcost[k], k, i, cost[k][i], i, lowcost[i]);
+                path_count[i]++;
+            }
+        }
+    }
 }
 
+
 int main(void) {
-    int m, n, c1, c2, ca, cb, rlength, max_hands;
-    int team[MAXN] = {0};
+    int n_city, n_path, beg_city, des_city;
+    int team[MAXN], cost[MAXN][MAXN], lowcost[MAXN];
+    int sum = 0;
 
-    memset(cost, -1, sizeof(cost));           // -1 means not accessible
+    for (int i = 0; i < MAXN; i++) {
+        for (int j = 0; j < MAXN; j++) {
+            cost[i][j] = INF;
+        }
+    }
 
-    scanf("%d%d%d%d", &m, &n, &c1, &c2);
-    for (int i = 0; i < m; i++) {
+    // input
+    scanf("%d%d%d%d", &n_city, &n_path, &beg_city, &des_city);
+    for (int i = 0; i < n_city; i++) {
         scanf("%d", &team[i]);
     }
-    for (int i = 0; i < n; i++) {
-        scanf("%d%d%d", &ca, &cb, &rlength);
-        cost[ca][cb] = rlength;
-        cost[cb][ca] = rlength;
+    int c1, c2, l;
+    for (int i = 0; i < n_path; i++) {
+        scanf("%d%d%d", &c1, &c2, &l);
+        cost[c1][c2] = l;
+        cost[c2][c1] = l;
+    }
+    //////
+
+    if (beg_city == des_city) {
+        printf("1 %d\n", team[beg_city]);
+        return 0;
     }
 
+    Dijkastra(cost, lowcost, n_city, beg_city);
 
+    int j = des_city;
+    while (pre[j] != -1) {
+        sum += team[j];
+        j = pre[j];
+    }
+    sum += team[beg_city];
 
+    // output
+    printf("%d %d\n", path_count[des_city] + 1, sum);
+    //////
 
-    // printf("%d %d\n", , max_hands);
     return 0;
 }
